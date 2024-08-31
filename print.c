ccaa1982eb8a41690ec5f9d75b4785b9d74a7935
johnnytan5 <108878474+johnnytan5@users.noreply.github.com>
2024-08-31 17:57:36 +0800

    Update print.c

--- print.c
+++ print.c
@@ -1,1 +1,17 @@
-void	
+void    printGrid(int **grid, int size)
+{
+    int i;
+    int j;
+
+    i = 0;
+    while (i < size)
+    {
+        j = 0;
+        while (j < size)
+        {
+            write(1, &grid[i][j], 1);
+            j++;
+        } 
+        i++;
+    }
+}	
